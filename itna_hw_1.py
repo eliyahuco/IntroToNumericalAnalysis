e64e69add07cf20b11c6181b24d277bf48e65ec7
eliyahuco <eliyahu.m.c@gmail.com>
2024-06-01 17:21:58 +0300

    commit

--- itna_hw_1.py
+++ itna_hw_1.py
@@ -8,13 +8,16 @@ This script is the HW_1 in the course intro to numerical analysis
 The objective of the script is find roots of a given polynom with several methods
 ---------------------------------------------------------------------------------
 
-Given polynom:  f(x) = xma
+Given polynom:  f(x) = X
 
 """
-
-
+#Labraries in use
 import numpy as np
 import math
 import matplotlib as plt
-print('hi')
-print('by')
+
+precision_requierd = 10**(-4)
+x_line = np.linspace(-5,3,10**4)
+
+
+

73a8319cfababf8e9035a8f0e764df4c0557525e
eliyahuco <eliyahu.m.c@gmail.com>
2024-06-01 15:18:49 +0300

    echo "# IntroToNumericalAnalysisHW1" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/eliyahuco/IntroToNumericalAnalysisHW1.git git push -u origin main

---
+++ itna_hw_1.py
@@ -0,0 +1,13 @@
+"""
+Author: Eliyahu cohen
+Email: cohen11@mail.tau.ac.il
+---------------------------------------------------------------------------------
+Short Description:
+
+This script is the HW_1 in the course intro to numerical analysis
+The objective of the script is find roots of a given polynom with several methods
+---------------------------------------------------------------------------------
+
+Given polynom:  f(x) = xma
+
+"""

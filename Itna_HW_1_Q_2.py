71162a3ca8b7854176a69ae13c7cfdd08dc5cc09
eliyahuco <eliyahu.m.c@gmail.com>
2024-06-18 20:25:07 +0300

    ready to submission

--- Itna_HW_1_Q_2.py
+++ Itna_HW_1_Q_2.py
@@ -29,19 +29,35 @@ epsilon = 10**(-4)
 
 # Define the functions f(x, y) and g(x, y)
 def f(x, y):
+    """
+    This function calculates the value of f(x, y) = 4y² + 4y - 52x - 1 in the given point (x, y).
+    """
     return 4*y**2 + 4*y - 52*x - 19
 
 def g(x, y):
+    """
+    This function calculates the value of g(x, y) = 169x² + 3y² - 111x - 10y in the given point (x, y).
+    """
     return 169*x**2 + 3*y**2 - 111*x - 10*y
 
 # Define the partial derivative of the functions f(x, y) and g(x, y)
 def partial_derivative_of_function_2d(function, x, y, h = 10**-9):
+    """"
+    This function calculates the partial derivative of a 2D function.
+    The function receives the function, the point (x, y) and the step size h.
+    The function returns the partial derivative of the function at the point (x, y).
+    """
     df_dx = (function(x + h, y) - function(x, y)) / h
     df_dy = (function(x, y + h) - function(x, y)) / h
     return df_dx, df_dy
 
 # Define the Newton-Raphson method for simultaneous solution
 def newton_raphson_method_for_simultaneous_solution( x0, y0, epsilon = 10**-4):
+    """"
+    This function solves the system of equations f(x, y) = 0 and g(x, y) = 0 using the Newton-Raphson method.
+    The function receives the initial guess (x0, y0) and the precision requirement epsilon.
+    The function returns the solution (x, y) of the system of equations.
+    """
     x = x0
     y = y0
     if abs(f(x, y)) < epsilon and abs(g(x, y)) < epsilon:
@@ -70,14 +86,25 @@ def main():
     print("The initial guess is: x0 = -0.01, y0 = -0.01")
     print("The precision requirement is: epsilon = 10⁻⁴")
     print("\n")
-    print("the value of f(x, y) anf g(x, y) at the initial guess is:")
+    print("the value of f(x, y) and g(x, y) at the initial guess is:")
     print("f(x0, y0) = ", f(x0, y0))
     print("g(x0, y0) = ", g(x0, y0), "\n")
-
     print("The solution of the system of equations is:")
-    print(newton_raphson_method_for_simultaneous_solution( x0, y0, epsilon))
+    solution_for_the_system = newton_raphson_method_for_simultaneous_solution( x0, y0, epsilon)
+    print("x = ", solution_for_the_system[0], ", y = ", solution_for_the_system[1])
+    print("\n")
+    print("The value of f(x, y) at the solution is:")
+    print("f(x, y) = ", f(solution_for_the_system[0], solution_for_the_system[1]))
+    print("The value of g(x, y) at the solution is:")
+    print("g(x, y) = ", g(solution_for_the_system[0], solution_for_the_system[1]))
+    print("the solution mathces the precision requirement of 10⁻⁴.")
 
     print("\n-------------------\n---Question 2.2:---\n-------------------")
+    print("The function to plot is: f(x, y) = sin(4y)cos(0.5x)")
+    print("The range of x is: -10 < x < 10")
+    print("The range of y is: -5 < y < 5")
+    print("the function will be plotted aotomatically.")
+    print("the graph will be saved as '3D_graph_f(x,y)=sin(4y)cos(0.5x).png' in the current directory, and will be added to the submission.")
 # Plot the 3D graph
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111, projection='3d')
@@ -87,7 +114,9 @@ def main():
     ax.set_zlabel('f(x, y)')
     ax.set_title('f(x, y) = sin(4y)cos(0.5x)')
 
-    # plt.show()
+    plt.show()
+    #save the plot
+    fig.savefig("3D_graph_f(x,y)=sin(4y)cos(0.5x).png")
 
 
 if __name__ == "__main__":

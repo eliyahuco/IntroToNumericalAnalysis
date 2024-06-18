745cbbdd292d94fd332baccd38afeb5606ddd58d
eliyahuco <eliyahu.m.c@gmail.com>
2024-06-18 21:16:33 +0300

    ready to submission

---
+++ HW_2/Itna_HW_2_Q_1.py
@@ -0,0 +1,9 @@
+"""
+Author: Eliyahu cohen
+Email: cohen11@mail.tau.ac.il
+---------------------------------------------------------------------------------
+Short Description:
+
+This script is the HW_2 Question 1 in the course intro to numerical analysis
+
+"""

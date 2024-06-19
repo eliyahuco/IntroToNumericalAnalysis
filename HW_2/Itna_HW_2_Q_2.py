5150537624472b60c6a835a81035176ae5885621
eliyahuco <eliyahu.m.c@gmail.com>
2024-06-19 21:50:52 +0300

    description

--- HW_2/Itna_HW_2_Q_2.py
+++ HW_2/Itna_HW_2_Q_2.py
@@ -8,3 +8,9 @@ This script is the HW_2 Question 1 in the course intro to numerical analysis
 the objective of this script is to finf the inverse of a matrix using the LU decomposition method
 and show the result to the user
 """
+# Libraries in use
+from tabulate import tabulate
+import numpy as np
+from itna_hw_2_q_1 import lu_decomposition_steps as lu_d
+from itna_hw_2_q_1 import rankin_matrix as rm
+

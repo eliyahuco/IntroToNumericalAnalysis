0ce7a25c2ecb5f59b4cd8e15e91bd78c75297729
eliyahuco <eliyahu.m.c@gmail.com>
2024-06-19 21:46:44 +0300

    description

---
+++ HW_2/Itna_HW_2_Q_2.py
@@ -0,0 +1,10 @@
+"""
+Author: Eliyahu cohen
+Email: cohen11@mail.tau.ac.il
+---------------------------------------------------------------------------------
+Short Description:
+
+This script is the HW_2 Question 1 in the course intro to numerical analysis
+the objective of this script is to finf the inverse of a matrix using the LU decomposition method
+and show the result to the user
+"""

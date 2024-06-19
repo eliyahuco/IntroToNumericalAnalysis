ed345a55048d3f6f29b045a6fdb5f6d51373e215
eliyahuco <eliyahu.m.c@gmail.com>
2024-06-19 23:34:37 +0300

    ready to submit

--- HW_2/Itna_HW_2_Q_2.py
+++ HW_2/Itna_HW_2_Q_2.py
@@ -28,7 +28,7 @@ def inverse_matrix_with_lu_decomposition(A):
     """
     This function calculates the inverse of a matrix using the LU decomposition method
     :param A: the matrix of coefficients
-    :return: the inverse of the matrix
+    :return: the inverse of the matrix and the L and U matrices
     the function uses the LU decomposition from HW_2 Question 1
     the name of the file is Itna_HW_2_Q_1.py
     """
@@ -37,7 +37,8 @@ def inverse_matrix_with_lu_decomposition(A):
     I = np.eye(n)# initialize the identity matrix
     for i in range(n):# loop over the columns of the identity matrix
         A_inverse[:, i] = hw1.lu_decomposition_steps(A, I[:, i])[0]# solve the system of linear equations
-    return A_inverse
+
+    return A_inverse, hw1.lu_decomposition_steps(A, I[:, i])[2], hw1.lu_decomposition_steps(A, I[:, i])[3]
 
 
 
@@ -45,19 +46,24 @@ def main():
     # calculate the inverse of the matrix
     A_inv = inverse_matrix_with_lu_decomposition(A)
     print("\nthe inverse of the matrix is:\n")
-    print(tabulate(A_inv, tablefmt='plain'))
+    print(tabulate(A_inv[0], tablefmt='plain'))
+    print("\nthe L matrix is from the LU decomposition is:\n")
+    print(tabulate(A_inv[1], tablefmt='plain'))
+    print("\nthe U matrix is from the LU decomposition is:\n")
+    print(tabulate(A_inv[2], tablefmt='plain'))
     print("\nlet's check the result:\n")
+
     print("\nThe inverse of the matrix using numpy is:\n")
     print(tabulate(np.linalg.inv(A), tablefmt='plain'))
     # check if the result is correct
     print("\nThe difference between the two results is:\n")
-    print(tabulate(abs(A_inv - np.linalg.inv(A)), tablefmt='plain'))
+    print(tabulate(abs(A_inv[0] - np.linalg.inv(A)), tablefmt='plain'))
     # check if the result is correct
     print("\nthe multiplication of the matrix and its inverse is:\n")
-    print(tabulate(np.dot(A, A_inv), tablefmt='plain'))
+    print(tabulate(np.dot(A, A_inv[0]), tablefmt='plain'))
     print("\n we see that we get very small numbers that are not exactly 0")
     print("\n after rounding the result is:\n")
-    print(tabulate(np.round(np.dot(A, A_inv)), tablefmt='plain'))
+    print(tabulate(np.round(np.dot(A, A_inv[0])), tablefmt='plain'))
     print("\n we can see that the result is the identity matrix")
     print("\n")
     print("the script has finished running")

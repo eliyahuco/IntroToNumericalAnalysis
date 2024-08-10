08713c03e9f335ee5e1ead9d47211ac9762c373d
eliyahuco <eliyahu.m.c@gmail.com>
2024-08-10 16:49:23 +0300

    ready to submit

--- HW_5/Itna_HW_5_Q_3.py
+++ HW_5/Itna_HW_5_Q_3.py
@@ -21,7 +21,6 @@ y(x) = 2 / (1 + x)^2
 
 We will compare the results of the methods and with the analytical solution.
 Also, we will plot the results.
-
 ---------------------------------------------------------------------------------
 """
 
@@ -50,23 +49,25 @@ def rk4_method_1st_order(ode_func, x0, y0, h, steps):
 
 # Adams-Bashforth Fourth Order Predictor
 def adams_bashforth_4th_order_predictor(ode_func, x_values, y_values, h):
-    return y_values[-1] + h / 24 * (
-            55 * ode_func(x_values[-1], y_values[-1])
-            - 59 * ode_func(x_values[-2], y_values[-2])
-            + 37 * ode_func(x_values[-3], y_values[-3])
-            - 9 * ode_func(x_values[-4], y_values[-4])
-    )
+    return y_values[-1] + h / 24 * (55 * ode_func(x_values[-1], y_values[-1])- 59 * ode_func(x_values[-2], y_values[-2])
+            + 37 * ode_func(x_values[-3], y_values[-3])- 9 * ode_func(x_values[-4], y_values[-4]))
 
 
 # Adams-Moulton Fourth Order Corrector
 def adams_moulton_4th_order_corrector(ode_func, x_values, y_values, y_pred, h):
-    return y_values[-1] + h / 24 * (
-            9 * ode_func(x_values[-1] + h, y_pred)
-            + 19 * ode_func(x_values[-1], y_values[-1])
-            - 5 * ode_func(x_values[-2], y_values[-2])
-            + ode_func(x_values[-3], y_values[-3])
-    )
+    return y_values[-1] + h / 24 * (9 * ode_func(x_values[-1] + h, y_pred) + 19 * ode_func(x_values[-1], y_values[-1])
+            - 5 * ode_func(x_values[-2], y_values[-2]) + ode_func(x_values[-3], y_values[-3]))
 
+def adam_bashforth_moulton_predictor_corrector(ode_func, x0, y0, h, xmax):
+    x_values, y_values = rk4_method_1st_order(ode_func, x0, y0, h, 3)
+
+    for _ in range(3, int((xmax - x0) / h)):
+        y_pred = adams_bashforth_4th_order_predictor(ode_func, x_values, y_values, h)
+        y_corr = adams_moulton_4th_order_corrector(ode_func, x_values, y_values, y_pred, h)
+        x_values = np.append(x_values, x_values[-1] + h)
+        y_values = np.append(y_values, y_corr)
+
+    return x_values, y_values
 
 def main():
     def ode_func(x, y):
@@ -88,31 +89,20 @@ def main():
     x_values, y_values = rk4_method_1st_order(ode_func, x0, y0, h, steps)
 
     # Adams-Bashforth-Moulton Predictor-Corrector
-    for _ in range(steps, int((xmax - x0) / h)):
-        # Predictor step (Adams-Bashforth)
-        y_pred = adams_bashforth_4th_order_predictor(ode_func, x_values, y_values, h)
-
-        # Corrector step (Adams-Moulton)
-        y_corr = adams_moulton_4th_order_corrector(ode_func, x_values, y_values, y_pred, h)
-
-        # Append new values
-        x_values = np.append(x_values, x_values[-1] + h)
-        y_values = np.append(y_values, y_corr)
+    x_values, y_values = adam_bashforth_moulton_predictor_corrector(ode_func, x0, y0, h, xmax)
 
     # Analytical solution
     x_analytical = np.linspace(x0, xmax, 400)
     y_analytical = analytical_solution(x_analytical)
 
     plt.figure(figsize=(10, 6))
-    plt.plot(x_values, y_values, label='Adams-Bashforth 4th Order Predictor & Adams-Moulton 4th Order Corrector',
-             marker='o')
-    plt.plot(x_analytical, y_analytical, 'r--', label='Analytical Solution')
+    plt.plot(x_values, y_values, label='Adams-Bashforth 4th Order Predictor & Adams-Moulton 4th Order Corrector',marker='o', color='g', linestyle='-', markersize=4, markerfacecolor='black')
+    plt.plot(x_analytical, y_analytical, '--', label='Analytical Solution', color='purple')
     plt.xlabel('x', fontweight='bold', fontsize=14)
     plt.ylabel('y', fontweight='bold', fontsize=14)
     plt.title(r'Solution of $\frac{dy}{dx} = \frac{-2y}{1+x}$', fontsize=14, fontweight='bold')
     plt.text(4.5, 1.5, r'$y(0) = 2$', fontsize=12, bbox=dict(facecolor='white', alpha=0.5))
-    plt.text(4.5, 1.7, "Analytical solution: " + r'$y(x) = \frac{2}{(1+x)^2}$', fontsize=12,
-             bbox=dict(facecolor='white', alpha=0.5))
+    plt.text(4.5, 1.7, "Analytical solution: " + r'$y(x) = \frac{2}{(1+x)^2}$', fontsize=12,bbox=dict(facecolor='white', alpha=0.5))
     plt.legend()
     plt.grid()
     plt.savefig('Adams_Bashforth_Moulton_Predictor_Corrector_and_Analytical_Solution_Plot_Q_3.png')
@@ -121,7 +111,6 @@ def main():
     print("\nThe script has finished running.")
     print("Thank you for using the script.")
     print("\n")
-
 
 if __name__ == '__main__':
     main()

d65e90bee5b3b538659f5cd477b6682de7b5e71c
eliyahuco <eliyahu.m.c@gmail.com>
2024-09-06 16:05:38 +0300

    ready to submit

---
+++ Fainal Project/FinalProject_Q1.py
@@ -0,0 +1,15 @@
+"""
+Author: Eliyahu Cohen
+Email: cohen11@mail.tau.ac.il
+---------------------------------------------------------------------------------
+Short Description:
+
+This script is question 1 in the final project for the course intro to numerical analysis
+
+the objective of this script is to solve a problem of the wave equation in 2D space:
+
+
+
+
+---------------------------------------------------------------------------------
+"""

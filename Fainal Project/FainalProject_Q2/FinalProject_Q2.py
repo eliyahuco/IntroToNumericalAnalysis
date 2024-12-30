7b5c518abb7adf78a5d905119e78bb5369fe0262
eliyahuco <eliyahu.m.c@gmail.com>
2024-12-30 14:58:22 +0200

    before main function

---
+++ Fainal Project/FainalProject_Q2/FinalProject_Q2.py
@@ -0,0 +1,11 @@
+"""
+Author: Eliyahu Cohen
+Email: cohen11@mail.tau.ac.il
+---------------------------------------------------------------------------------
+Description:
+
+This script is question 2 in the final project for the course intro to numerical analysis
+
+
+
+"""
